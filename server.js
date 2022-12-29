fb67cf9eb562e4ffdcc32bad0102121bbc7120dd
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-29 21:27:49 +0900

    サーバー準備 & HTMLコピー

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(PORT, () => {
+  console.log("listening on 3000")
+})
